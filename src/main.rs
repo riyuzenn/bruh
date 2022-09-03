00f65f18277770e0d63d21a0d06a82f08c1199b0
riyuzenn <riyuzenn@gmail.com>
2022-09-03 16:18:34 +0200

    ✨ feat: Add doc menu

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ fn main() {
         String::from("🚧 prog:  New features in progress"),
         String::from("⚒️  fix:   Fix a bug or any errors"),
         String::from("🎨 style: Format or style files"),
-        String::from("♻️  ref:   Refactor code")
+        String::from("♻️  ref:   Refactor code"),
+        String::from("📝 doc:  Update any documentation related file")
     ];
+
     let bruh = bruh::BruhCommit::new("Select commit type to be use", item);
     bruh.exec().unwrap();
 
